bdf09331310ee68dee23dfe2b429d8fed9c6fb47
Simon (darkside) Jackson <darkside@zenithmoon.com>
2021-03-11 21:12:06 +0000

    Updated and tested with Core/Examples

---
+++ Editor/glTFPathFinder.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using XRTK.Editor.Utilities;
+
+namespace XRTK.glTF.Editor
+{
+    /// <summary>
+    /// Dummy scriptable object used to find the relative path of the com.xrtk.glTF.
+    /// </summary>
+    /// <inheritdoc cref="IPathFinder" />
+    public class glTFPathFinder : ScriptableObject, IPathFinder
+    {
+        /// <inheritdoc />
+        public string Location => $"/Editor/{nameof(glTFPathFinder)}.cs";
+    }
+}
