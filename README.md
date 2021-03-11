b6724ba60965fa7576c92900ae9a7b902a562678
Simon (darkside) Jackson <darkside@zenithmoon.com>
2021-03-11 18:05:08 +0000

    Initial project check-in.

---
+++ README.md
@@ -0,0 +1,3 @@
+# glTF
+
+The glTF package for the [XRTK - Mixed Reality Toolkit](https://github.com/XRTK/XRTK-Core).
---
+++ Runtime/Providers/Controllers/glTFController.cs
@@ -0,0 +1,12 @@
+// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using XRTK.Providers.Controllers;
+
+namespace XRTK.glTF.Providers.Controllers
+{
+    [System.Runtime.InteropServices.Guid("168a9236-94bd-4ff9-bf8e-3e0f62329291")]
+    public class glTFController : BaseController
+    {
+    }
+}
