c6e72c7c30818a2314601426907b1ba14bec2ca9
Stephen Hodgson <hodgson.designs@gmail.com>
2021-05-16 13:26:25 -0400

    added more assembly info

--- XRTK.glTF/Packages/com.xrtk.gltf/Runtime/AssemblyInfo.cs
+++ XRTK.glTF/Packages/com.xrtk.gltf/Runtime/AssemblyInfo.cs
@@ -1,4 +1,11 @@
-﻿// Copyright (c) XRTK. All rights reserved.
+// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 #define XRTK_GLTF
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.2.3")]
+[assembly: AssemblyTitle("com.xrtk.gltf")]
+[assembly: AssemblyCompany("XRTK")]
+[assembly: AssemblyCopyright("Copyright (c) XRTK. All rights reserved.")]
